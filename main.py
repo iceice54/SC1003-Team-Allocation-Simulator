f083e5892a8c16bee7483099961d28ca77ae8bc2
iceice54 <125724815+iceice54@users.noreply.github.com>
2024-10-17 16:35:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-import read_to_dict
+from read_to_dict import read_file_create_dictionary
 
-student_data = read_to_dict.read_file_create_dictionary("records copy.csv")
+student_data = read_file_create_dictionary("records.csv")
